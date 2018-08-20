88e9e8d7d1f83664270f7b8c2d05275da427a1d5
MurrTuck <murrtuck@gmail.com>
2018-08-20 00:03:51 -0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+window.addEventListener("load", init);
+
+let time = 5;
+let score = 0;
+let isPlaying;
+
+// DOM Elements
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
+
+const words = [
+  "hat",
+  "river",
+  "lucky",
+  "statue",
+  "generate",
+  "stubborn",
+  "cocktail",
+  "runaway",
+  "joke",
+  "developer",
+  "establishment",
+  "hero",
+  "javascript",
+  "nutrition",
+  "revolver",
+  "echo",
+  "siblings",
+  "investigate",
+  "horrendous",
+  "symptom",
+  "laughter",
+  "magic",
+  "master",
+  "space",
+  "definition"
+];
+
+//Initialize Game
+function init() {
+  // Load word from array
+  showWord(words);
+  // Call countdown every second
+  setInterval(countdown, 1000);
+}
+
+// Pick & show random word
+function showWord(words) {
+  // Generate random array index
+  const randIndex = Math.floor(Math.random() * words.length);
+  // Output random word
+  currentWord.innerHTML = words[randIndex];
+}
+
+// Countdown timer
+function countdown() {
+  // Make sure time is ont run out
+  if (time > 0) {
+    // Decrement
+    time--;
+  } else if (time === 0) {
+    //Game is over
+    isPlaying = false;
+  }
+
+  //Show time
+  timeDisplay.innerHTML = time;
+}
